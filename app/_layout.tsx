15b3e6708c907247843131aed12a127c8f1bbf9b
Khushi Ray <khushiray972@gmail.com>
2026-05-15 13:40:41 +0530

    first screen done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,19 +5,23 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen 
+          name="index" 
+          options={{ headerShown: false }} 
+        />
+
+        <Stack.Screen 
+          name="modal" 
+          options={{ presentation: 'modal', title: 'Modal' }} 
+        />
       </Stack>
+
       <StatusBar style="auto" />
     </ThemeProvider>
   );
